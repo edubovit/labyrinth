a7036856dc49872857cb1d33732b087a20785198
edubovit <Evgeny.Dubovitsky@altezza.org>
2022-06-10 00:26:08 +0300

    prettify GameSessionDTO

--- src/main/java/net/edubovit/labyrinth/dto/GameSessionDTO.java
+++ src/main/java/net/edubovit/labyrinth/dto/GameSessionDTO.java
@@ -1,14 +1,31 @@
 package net.edubovit.labyrinth.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 import java.util.UUID;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record GameSessionDTO(UUID id, String mapUrl, LabyrinthDTO map, PlayerCoordinates playerCoordinates,
-                             int turns, Boolean finish, Boolean successMove) {
+public record GameSessionDTO(UUID id,
+                             String mapUrl,
+                             @JsonUnwrapped LabyrinthDTO map,
+                             PlayerCoordinates playerCoordinates,
+                             int turns,
+                             Boolean finish,
+                             Boolean successMove) {
 
     public record PlayerCoordinates(int x, int y, int i, int j) {
+    }
+
+    @Override
+    public String toString() {
+        return "GameSessionDTO{" +
+                "id=" + id +
+                ", playerCoordinates=" + playerCoordinates +
+                ", turns=" + turns +
+                ", finish=" + finish +
+                ", successMove=" + successMove +
+                '}';
     }
 
 }
