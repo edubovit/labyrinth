17565b160c104d2c7abd70fdea7e8396a32d4136
edubovit <Evgeny.Dubovitsky@altezza.org>
2022-06-09 20:54:49 +0300

    fix CORS configuration

--- src/main/java/net/edubovit/labyrinth/config/WebConfiguration.java
+++ src/main/java/net/edubovit/labyrinth/config/WebConfiguration.java
@@ -3,22 +3,30 @@ package net.edubovit.labyrinth.config;
 import net.edubovit.labyrinth.config.properties.ApplicationProperties;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebMvc
 @RequiredArgsConstructor
+@Slf4j
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final ApplicationProperties applicationProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(applicationProperties.getAllowedOrigins());
+        String[] allowedOrigins = applicationProperties.getAllowedOrigins();
+        log.info("Allowed origins: {}", Arrays.toString(allowedOrigins));
+        if (allowedOrigins != null && allowedOrigins.length > 0) {
+            registry.addMapping("/**")
+                    .allowedOrigins(allowedOrigins);
+        }
     }
 
 }
