bc1ec2c05a09e3a074458e5cc67fd27ca4884435
edubovit <Evgeny.Dubovitsky@altezza.org>
2022-06-14 16:12:17 +0300

    PostgreSQL 12 compatible UUID generation

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,11 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE "user" (
-    id        UUID         PRIMARY KEY DEFAULT gen_random_uuid(),
+    id        UUID         PRIMARY KEY DEFAULT uuid_generate_v4(),
     username  VARCHAR(40)  NOT NULL UNIQUE,
     password  TEXT         NOT NULL,
     game_id   UUID
 );
 
 CREATE TABLE game (
-    id         UUID   PRIMARY KEY DEFAULT gen_random_uuid(),
+    id         UUID   PRIMARY KEY DEFAULT uuid_generate_v4(),
     game_blob  bytea  NOT NULL
 );
